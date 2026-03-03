c2cd811639aa27c8628bbd5f650d5180965dc8c6
cyrox007 <cyrox007@gmail.com>
2026-03-03 14:40:18 +0300

    лишние импорты

--- frontend/src/components/UI/Modal.vue
+++ frontend/src/components/UI/Modal.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script setup>
-import { defineProps, defineEmits, computed } from 'vue'
+import { defineEmits, computed } from 'vue'
 
 const props = defineProps({
 	isOpen: Boolean,
