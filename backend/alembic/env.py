bf67d144663f64668e8f7b3f9517c296a55016b9
cyrox007 <cyrox007@gmail.com>
2026-04-20 11:29:47 +0300

    рефактор 2

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -23,7 +23,7 @@ config.set_main_option('sqlalchemy.url', cfg.database_url())
 # target_metadata = mymodel.Base.metadata
 from models.users import User, UserRoleAssociation
 target_metadata = User.__table__.metadata
-from models.permissions import Permission
+from models.permissions_model import Permission
 from models.tariffs_model import TariffPlan, TariffLimit
 from models.subscription_model import Subscription
 from models.tokens import APIToken
