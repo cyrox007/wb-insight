b2ce8e1de79f28fe225755dfe03f146b39eb8e1c
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-14 16:47:48 +0300

    P9: pass dashboard account filters

--- frontend/src/API/Dashboard/DashboardService.js
+++ frontend/src/API/Dashboard/DashboardService.js
@@ -1,8 +1,8 @@
 import $api from "..";
 
 export default class DashboardService {
-    static async get_dashboard_data() {
-        return await $api.get("/dashboard/");
+    static async get_dashboard_data(params = {}) {
+        return await $api.get("/dashboard/", { params });
     }
 
     static async get_dashboard_charts(params = {}) {
