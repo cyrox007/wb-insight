68117a2467e6d77199fb56d208613ff16cb79851
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-14 16:48:06 +0300

    P9: pass unit economy account filters

--- frontend/src/API/Dashboard/UnityEconomyService.js
+++ frontend/src/API/Dashboard/UnityEconomyService.js
@@ -1,7 +1,7 @@
 import $api from "..";
 
 export default class UnityEconomyService {
-    static async get_unity_economy_table() {
-        return await $api.get('/dashboard/unity')
+    static async get_unity_economy_table(params = {}) {
+        return await $api.get('/dashboard/unity', { params })
     }
 }
