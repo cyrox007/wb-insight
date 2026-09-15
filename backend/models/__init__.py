ca109dfc1086528bfdf4d01f85e4600ccd1b7a74
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-15 13:38:16 +0300

    P17: export price models

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -7,6 +7,7 @@ from .wb_advertising_stats import WbAdvertisingStats
 from .wb_operational import WbOrder, WbSale
 from .wb_sales_funnel import WbSalesFunnelDaily
 from .wb_paid_storage import WbPaidStorage
+from .wb_price import WbPriceCurrent, WbPriceChange
 from .product_cost_price_model import ProductCostPrice
 from .product_cost_price_history import ProductCostPriceHistory
 from .manual_expense import ManualExpense
