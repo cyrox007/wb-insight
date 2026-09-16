deb0adf423559708da423e154791dc4754ec1033
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-16 16:30:49 +0300

    Normalize role management API routes

--- frontend/src/API/ControlPanel/CP_Roles.js
+++ frontend/src/API/ControlPanel/CP_Roles.js
@@ -2,10 +2,10 @@ import $api from "..";
 
 export default class CP_Roles {
     static async getRolesList() {
-        return await $api.get("/control-panel/roles");
+        return await $api.get("/control-panel/roles/");
     }
     static async assignRoleToUser(user_id, role) {
-        return await $api.post("/control-panel/roles", {user_id, role});
+        return await $api.post("/control-panel/roles/", {user_id, role});
     }
     static async deleteRoleFromUser(user_id, role_code) {
         return await $api.delete(`/control-panel/roles/${user_id}/${role_code}`)
