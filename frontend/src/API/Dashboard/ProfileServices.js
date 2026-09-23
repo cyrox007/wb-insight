c326580cd420a8f12be829849e28c40984ca4115
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-23 18:59:05 +0300

    P97: добавить API отмены pending email

--- frontend/src/API/Dashboard/ProfileServices.js
+++ frontend/src/API/Dashboard/ProfileServices.js
@@ -13,6 +13,10 @@ export default class ProfileServices {
         return await $api.post('/dashboard/profile/email-change/request', { email });
     }
 
+    static async cancelEmailChange() {
+        return await $api.post('/dashboard/profile/email-change/cancel', {});
+    }
+
     static async checkTokenPermission(user_id, tariff_id) {
         return await $api.get(`/dashboard/profile/check-token-permission/${user_id}`, {
             params: {
